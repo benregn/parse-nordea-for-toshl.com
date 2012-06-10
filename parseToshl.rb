71b12d8ebd3368db379cb324cfc4950af77146c7
Tomas Thor Jonsson <benregn@gmail.com>
2012-06-10 18:21:20 +0200

    prepare Toshl csv

---
+++ parseToshl.rb
@@ -0,0 +1,23 @@
+# encoding: utf-8
+require 'CSV'
+require "./helpers"
+
+include Helpers
+
+def prepare_CSV
+  rows = []
+  columns_to_remove = ["Income amount", "Currency", "Description"]
+  switch_keys = { "Entry (tags)" => "Tag", "Expense amount" => "Amount" }
+
+  CSV.foreach("./toshl_export.csv", col_sep: ',', headers: true) do |row|
+    row = Helpers.change_key_name row, switch_keys
+    columns_to_remove.each { |column| row.delete column }
+
+    row = row.to_hash
+    if not row["Amount"].empty?
+      # puts row
+      rows.push row
+    end
+  end
+  return rows
+end
